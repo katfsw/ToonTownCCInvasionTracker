1bac9ed4760f15156843c0dd299b4256c6329c17
BrandyBecker <brandybeckerfsw@gmail.com>
2020-02-17 23:18:48 -0500

    Some styling changes - adding a Groups component

---
+++ invasion-app/src/components/Groups.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Groups = () => {
+        return (
+            <div class="group-boarding">
+                <h3> Test </h3>
+            </div>
+        )
+}
+
+export default Groups;
--- invasion-app/src/components/Nav.js
+++ invasion-app/src/components/Nav.js
@@ -1,17 +1,48 @@
 import React from 'react'
+import Groups from './Groups'
+import {Link} from 'react-router-dom'
+
 
 const Nav = () => {
         return (
-            <div class="nav-footer nav-text">
-  
-                <a alt="Toon Town Corporate Clash Home Page" href="https://corporateclash.net/">Home</a>
+            <div class="nav-footer nav-text">  
+                <Link to="/">Home</Link>
                 <a href="https://corporateclash.net/news">News</a>
                 <a href="https://corporateclash.net/releases">Releases</a>
                 <a href="https://corporateclash.net/comics">Comics</a>
-
-
+                <Link to="/groups">Groups</Link> 
             </div>
         )
 }
 
 export default Nav;
+
+/*
+ <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="https://corporateclash.net/">Home</Link>
+          </li>
+          <li>
+            <Link to="https://corporateclash.net/news">News</Link>
+          </li>
+          <li>
+            <Link to="https://corporateclash.net/releases">Releases</Link>
+          </li>
+           <li>
+            <Link to="https://corporateclash.net/releases">Comics</Link>
+          </li>
+           <li>
+            <Link to="/groups">Groups</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/groups">
+            <Groups />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+*/
